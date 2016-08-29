b05010fa59435637879e8aff6e96e661019b11b7
liran <wangxiumei>
2016-08-29 20:00:21 +0800

    Initial Commit

---
+++ MemoryLeaks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemoryLeaks
+//
+//  Created by alicia on 8/29/16.
+//  Copyright © 2016 LeafTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MemoryLeaks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MemoryLeaks
+//
+//  Created by alicia on 8/29/16.
+//  Copyright © 2016 LeafTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
